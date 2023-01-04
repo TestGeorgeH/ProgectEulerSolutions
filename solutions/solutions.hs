c0b50cfbe7e2ac574b5f223c458a41b64b3886e1
TestGeorgeH <george30032002@gmail.com>
2023-01-04 04:18:12 +0400

    ormolu -ed the code

--- solutions/solutions.hs
+++ solutions/solutions.hs
@@ -1,32 +1,33 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 
-import Data.List
-import Data.Ord
 import Data.FastDigits (digits, undigits)
-import Math.NumberTheory.Roots (integerSquareRoot, isSquare)
+import Data.List
 import Data.Map (Map)
 import qualified Data.Map as Map
-
 import Data.Map.Internal.Debug (showTreeWith)
+import Data.Ord
+import Math.NumberTheory.Roots (integerSquareRoot, isSquare)
 
 -- TODO:
---     add normal tests 
+--     add normal tests
 --     add signatures for functions
 
 primes :: (Integral a) => [a]
-primes = sieve [2..]
-    where   sieve (p:xs) = p : sieve ( filter (`notDivisibleBy` p) xs )
-            sieve [] = []
-            notDivisibleBy t p = t `rem` p /= 0
+primes = sieve [2 ..]
+  where
+    sieve (p : xs) = p : sieve (filter (`notDivisibleBy` p) xs)
+    sieve [] = []
+    notDivisibleBy t p = t `rem` p /= 0
 
 primesOpt :: (Integral a) => [a]
-primesOpt = filter isPrime [2..]
+primesOpt = filter isPrime [2 ..]
+
 isPrime n = check n 2
-    where
-        check n d
-            | d*d > n        = True
-            | n `rem` d == 0 = False
-            | otherwise      = check n (d+1)
+  where
+    check n d
+      | d * d > n = True
+      | n `rem` d == 0 = False
+      | otherwise = check n (d + 1)
 
 -----------------------------
 -- Largest prime factor
@@ -35,12 +36,13 @@ isPrime n = check n 2
 
 largestPrimeFactorOf :: (Integral a) => a -> a
 largestPrimeFactorOf n = divider n primesOpt
-    where divider n (q:xs)
-            | n == q = q
-            | n `rem` q == 0 = divider (n `div` q) xs
-            | otherwise = divider n xs
-          
-          divider n [] = n  
+  where
+    divider n (q : xs)
+      | n == q = q
+      | n `rem` q == 0 = divider (n `div` q) xs
+      | otherwise = divider n xs
+    divider n [] = n
+
 -----------------------------
 -- 10001st prime
 -- Problem 7
@@ -53,7 +55,7 @@ tenThousandAndFirstPrime = primesOpt !! 10001
 -- Problem 10
 
 primesUntil :: (Integral a) => a -> [a]
-primesUntil upperBound = takeWhile ( <= upperBound) primesOpt
+primesUntil upperBound = takeWhile (<= upperBound) primesOpt
 
 primesSumUntil :: (Integral a) => a -> a
 primesSumUntil upperBound = sum $ primesUntil upperBound
@@ -67,39 +69,42 @@ numDigits n = floor (logBase 10 (fromIntegral n)) + 1
 
 rotateNumber :: (Integral a) => a -> a
 rotateNumber n = numHead + numTail
-    where numHead = (n `rem` 10) * 10 ^(numDigits n -1)
-          numTail = (n `div` 10)
+  where
+    numHead = (n `rem` 10) * 10 ^ (numDigits n - 1)
+    numTail = (n `div` 10)
 
 rotationsOfNumber :: (Integral a) => a -> [a]
 rotationsOfNumber n = rotationsHelper n (numDigits n)
-    where rotationsHelper n 0 = []
-          rotationsHelper n i = n : (rotationsHelper (rotateNumber n) (i-1))
+  where
+    rotationsHelper n 0 = []
+    rotationsHelper n i = n : (rotationsHelper (rotateNumber n) (i - 1))
 
--- A circular prime with at least two digits 
+-- A circular prime with at least two digits
 --     can only consist of combinations of the digits
---     1, 3, 7 or 9, because having 0, 2, 4, 6 or 8 as the last digit 
---     makes the number divisible by 2, 
+--     1, 3, 7 or 9, because having 0, 2, 4, 6 or 8 as the last digit
+--     makes the number divisible by 2,
 --     and having 0 or 5 as the last digit
 --     makes it divisible by 5.
 --     Wikipedia
 
 circularPrimesCandUntil :: (Integral a) => a -> [a]
 circularPrimesCandUntil n = filter canBeCand (primesUntil n)
-    where   
-            canBeCand :: (Integral a) => a -> Bool
-            canBeCand n = not (any (n `hasDigit`) [0,2,4,5,6,8])
-            hasDigit :: (Integral a) => a -> Int -> Bool
-            n `hasDigit` i = elem i (digits 10 (fromIntegral n :: Integer))
+  where
+    canBeCand :: (Integral a) => a -> Bool
+    canBeCand n = not (any (n `hasDigit`) [0, 2, 4, 5, 6, 8])
+    hasDigit :: (Integral a) => a -> Int -> Bool
+    n `hasDigit` i = elem i (digits 10 (fromIntegral n :: Integer))
 
 circularPrimesUntil :: (Integral a) => a -> [a]
 circularPrimesUntil n = concat $ circularPrimesUntil' 2 (circularPrimesCandUntil n)
-    where   rot c = rotationsOfNumber c
-            calculateRest c candidates = (circularPrimesUntil' (head (candidates \\ (rot c))) (candidates \\ (rot c)))
+  where
+    rot c = rotationsOfNumber c
+    calculateRest c candidates = (circularPrimesUntil' (head (candidates \\ (rot c))) (candidates \\ (rot c)))
 
-            circularPrimesUntil' _ [] = []
-            circularPrimesUntil' c candidates
-                | all (\t -> elem t candidates) (rot c) = (rot c) : (calculateRest c candidates)
-                | otherwise = (calculateRest c candidates)
+    circularPrimesUntil' _ [] = []
+    circularPrimesUntil' c candidates
+      | all (\t -> elem t candidates) (rot c) = (rot c) : (calculateRest c candidates)
+      | otherwise = (calculateRest c candidates)
 
 -----------------------------
 -- Prime permutations
@@ -115,35 +120,37 @@ fourDigitPrimes = leaveNDigitNumbers 4 (primesUntil 10000)
 rmdups :: (Ord a) => [a] -> [a] -- they are just 4 digits, that will cut it
 rmdups = map head . group . sort
 
-mapExample = helper [1,2,3,4,5]
-    where 
-            helper [] = Map.empty
-            helper (x:xs) = Map.insert x x (helper xs)  
+mapExample = helper [1, 2, 3, 4, 5]
+  where
+    helper [] = Map.empty
+    helper (x : xs) = Map.insert x x (helper xs)
 
 genKey :: (Integral a) => a -> a
 genKey n = fromIntegral $ undigits 10 $ sort $ digits 10 (fromIntegral n)
 
 keyedFourDigitPrimes :: (Integral a) => Map a [a]
 keyedFourDigitPrimes = mapper fourDigitPrimes
-    where   mapper :: (Integral a) => [a] -> Map a [a]
-            mapper [] = Map.empty
-            mapper (n:xs)
-                | isPrime n = Map.insertWith (++) (genKey n) [n] (mapper xs)
-                | otherwise = mapper xs 
+  where
+    mapper :: (Integral a) => [a] -> Map a [a]
+    mapper [] = Map.empty
+    mapper (n : xs)
+      | isPrime n = Map.insertWith (++) (genKey n) [n] (mapper xs)
+      | otherwise = mapper xs
 
 prettyFourDigitPrimesCandidates :: (Integral a) => [[a]]
-prettyFourDigitPrimesCandidates = map (sort.snd) $ Map.toList keyedFourDigitPrimes
+prettyFourDigitPrimesCandidates = map (sort . snd) $ Map.toList keyedFourDigitPrimes
 
 -- can be done faster, but sequences are no longer then 10
 
 isArithmSeq :: (Num a, Eq a) => [a] -> Bool
 isArithmSeq [] = False
 isArithmSeq [x] = False
-isArithmSeq [x,y] = True
-isArithmSeq (x:y:z:xs) = (x - y) == (y - z) && isArithmSeq (y:z:xs)
+isArithmSeq [x, y] = True
+isArithmSeq (x : y : z : xs) = (x - y) == (y - z) && isArithmSeq (y : z : xs)
 
 arithSubseq :: (Integral a) => [a] -> [[a]]
 arithSubseq seq = filter isArithmSeq $ subsequences seq
+
 maxArithSubseq :: (Integral a) => [a] -> [a]
 maxArithSubseq [] = []
 maxArithSubseq [x] = [x]
@@ -151,11 +158,11 @@ maxArithSubseq seq = maximumBy (comparing length) $ arithSubseq seq
 
 -- optimised to a couple of seconds
 largestPrettyFourDigitPrimes = filter (\t -> length t == maxLen) candidates
-    where   
-            candidates :: (Integral a) => [[a]]
-            candidates = map maxArithSubseq  prettyFourDigitPrimesCandidates
-            maxLen :: (Integral a) => a
-            maxLen = fromIntegral $ maximum $ map length candidates
+  where
+    candidates :: (Integral a) => [[a]]
+    candidates = map maxArithSubseq prettyFourDigitPrimesCandidates
+    maxLen :: (Integral a) => a
+    maxLen = fromIntegral $ maximum $ map length candidates
 
 -------------------------
 -- primes are boring, I'll go do something else
@@ -165,11 +172,12 @@ largestPrettyFourDigitPrimes = filter (\t -> length t == maxLen) candidates
 
 tenDigitSelfPower :: (Integral a) => a -> a
 tenDigitSelfPower n = helper n n
-    where   helper n 0 = 1   
-            helper n i = ((helper n (i-1)) * n) `rem` 10^(10 +1)
+  where
+    helper n 0 = 1
+    helper n i = ((helper n (i - 1)) * n) `rem` 10 ^ (10 + 1)
 
 tenDigitSelfPowerSum :: (Integral a) => a -> a
-tenDigitSelfPowerSum n = sum [tenDigitSelfPower i | i <- [1..n]]
+tenDigitSelfPowerSum n = sum [tenDigitSelfPower i | i <- [1 .. n]]
 
 -------------------------
 -- 1000-digit Fibonacci number
@@ -179,14 +187,15 @@ tenDigitSelfPowerSum n = sum [tenDigitSelfPower i | i <- [1..n]]
 
 fib :: (Integral a) => [a]
 fib = 1 : next
-    where next = 1 : zipWith (+) fib next
+  where
+    next = 1 : zipWith (+) fib next
 
 -- Explanation -- this is just a very fancy way of adding two numbers every time it's necessary
--- Cash is included, so it's linear 
+-- Cash is included, so it's linear
 
 -- returns # and fib number itself
 nDigitFibNumber :: (Integral a) => a -> (a, a)
-nDigitFibNumber n = last $ zip [1..] $ takeWhile (\t -> (t `div` 10^n) == 0 ) fib
+nDigitFibNumber n = last $ zip [1 ..] $ takeWhile (\t -> (t `div` 10 ^ n) == 0) fib
 
 numberOfThousandDigitFibNumber :: (Integral a) => a
 numberOfThousandDigitFibNumber = fst $ nDigitFibNumber 1000
@@ -196,19 +205,20 @@ numberOfThousandDigitFibNumber = fst $ nDigitFibNumber 1000
 -- Pentagon numbers
 -- Problem 44
 
-pentNumbers = [n*(3*n-1) `div` 2 | n <- [1..]]
+pentNumbers = [n * (3 * n - 1) `div` 2 | n <- [1 ..]]
 
-isPentNumber n 
-    | not $ isSquare (1+24*n) = False
-    | otherwise = (integerSquareRoot (1+24*n) `rem` 6) == 5
+isPentNumber n
+  | not $ isSquare (1 + 24 * n) = False
+  | otherwise = (integerSquareRoot (1 + 24 * n) `rem` 6) == 5
 
 cutePentNumbers = helper 2
-    where helper n = 
-            let pnums = take n pentNumbers
-                pn = last pnums
-                candidates = [sort [pn - i, i] | i <- init pnums]
-                filtered = filter (\t -> isPentNumber(last t) && isPentNumber(last t - head t)) candidates
-            in (filtered) ++ (helper (n+1))
+  where
+    helper n =
+      let pnums = take n pentNumbers
+          pn = last pnums
+          candidates = [sort [pn - i, i] | i <- init pnums]
+          filtered = filter (\t -> isPentNumber (last t) && isPentNumber (last t - head t)) candidates
+       in (filtered) ++ (helper (n + 1))
 
 smallestCuteNumber = head $ map (\t -> (last t) - (head t)) cutePentNumbers
 
@@ -217,7 +227,7 @@ smallestCuteNumber = head $ map (\t -> (last t) - (head t)) cutePentNumbers
 -- Digit factorials
 -- Problem 34
 digitFactorials :: (Integral a) => [a]
-digitFactorials = [1,1,2,6,24,120,720,5040,40320,362880]
+digitFactorials = [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880]
 
 -- number of digits d  of number n lies in 10^(d-1) ≤ n ≤ 9!d, and it holds for d ≤ 7
 -- Upper bound is 2_540_160, as largest sum of factorials is 9!*7
@@ -227,21 +237,21 @@ sumFactDigits :: (Integral a) => a -> a
 sumFactDigits n = sum $ map ((!!) digitFactorials) (digits 10 (fromIntegral n))
 
 specialFactNumbers :: (Integral a) => [a]
-specialFactNumbers = [i | i <- [0..2540160], sumFactDigits i == i]
+specialFactNumbers = [i | i <- [0 .. 2540160], sumFactDigits i == i]
 
 -- Longest Collatz sequence
 -- Problem 14
 
 collatz :: (Integral a) => a -> [a]
 collatz n
-    | n == 1 = [n]
-    | n `rem` 2 == 0 = n : collatz (n `div` 2)
-    | otherwise = n : collatz (3*n +1)
+  | n == 1 = [n]
+  | n `rem` 2 == 0 = n : collatz (n `div` 2)
+  | otherwise = n : collatz (3 * n + 1)
 
 longestCollatzUnder :: (Integral a) => a -> [a]
-longestCollatzUnder n = maximumBy (comparing length) [collatz i | i <- [1..n]]
+longestCollatzUnder n = maximumBy (comparing length) [collatz i | i <- [1 .. n]]
 
-main :: IO()
+main :: IO ()
 main = putStrLn (show (largestPrettyFourDigitPrimes))
 
 -- main :: IO()
